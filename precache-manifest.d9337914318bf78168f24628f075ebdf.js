8b09806dbea6c02f4b4e5e809a63fbcd6fa1d106
Evilinos <e-streltsov@mail.ru>
2020-06-08 12:10:29 +0300

    Updates

---
+++ precache-manifest.d9337914318bf78168f24628f075ebdf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc7ccfb4eea3c5800a77d47c6387ee4",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "442332f50a3e8aa1c5fd",
+    "url": "/Weather-app/static/css/main.c3fa618f.chunk.css"
+  },
+  {
+    "revision": "df557a0ed3c9535f02c2",
+    "url": "/Weather-app/static/js/2.97f74f66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Weather-app/static/js/2.97f74f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442332f50a3e8aa1c5fd",
+    "url": "/Weather-app/static/js/main.f035d912.chunk.js"
+  },
+  {
+    "revision": "9d2b0e576db66cd702e0",
+    "url": "/Weather-app/static/js/runtime-main.6d0542b5.js"
+  },
+  {
+    "revision": "bf8c41acac6ec5741fb1ac25edd94ced",
+    "url": "/Weather-app/static/media/MainBackground.bf8c41ac.jpg"
+  },
+  {
+    "revision": "2d4067170f49890b7942c72b0a1599cd",
+    "url": "/Weather-app/static/media/arrow.2d406717.svg"
+  },
+  {
+    "revision": "cff3c08e952ec834ac64a2c119be7a40",
+    "url": "/Weather-app/static/media/background.cff3c08e.jpg"
+  },
+  {
+    "revision": "ae81618c3d5391780b70925e62d7d799",
+    "url": "/Weather-app/static/media/background2.ae81618c.jpg"
+  },
+  {
+    "revision": "735baf439613cceb550af55222af9bce",
+    "url": "/Weather-app/static/media/barometer.735baf43.svg"
+  },
+  {
+    "revision": "47e07357ab9bec29b4149c727ff87725",
+    "url": "/Weather-app/static/media/cloud.47e07357.svg"
+  },
+  {
+    "revision": "ad8b6b6453aa2b47a53377afd600612b",
+    "url": "/Weather-app/static/media/latitude.ad8b6b64.svg"
+  },
+  {
+    "revision": "3694f6a8d6bd6175a0004f8936559b95",
+    "url": "/Weather-app/static/media/longitude.3694f6a8.svg"
+  },
+  {
+    "revision": "c6f4b1b573bf0525cd0c5a8f64e1bf3e",
+    "url": "/Weather-app/static/media/preloader.c6f4b1b5.svg"
+  },
+  {
+    "revision": "6f01c3666da71dd77b14e1a9648c3ac9",
+    "url": "/Weather-app/static/media/refresh.6f01c366.svg"
+  },
+  {
+    "revision": "4ead0e6d5edb91ccf2f92f6d90902852",
+    "url": "/Weather-app/static/media/solar-radiation.4ead0e6d.svg"
+  },
+  {
+    "revision": "6253757f6947870a6bc34c64dad28a83",
+    "url": "/Weather-app/static/media/sunrise.6253757f.svg"
+  },
+  {
+    "revision": "8ac8ca6d0b8a836eb11beba3a7e6f265",
+    "url": "/Weather-app/static/media/sunset.8ac8ca6d.svg"
+  },
+  {
+    "revision": "e5ff57d832706d95d189115e781723e3",
+    "url": "/Weather-app/static/media/thermometer.e5ff57d8.svg"
+  },
+  {
+    "revision": "b15b3746b5f450f50da2cd4f07bc8d87",
+    "url": "/Weather-app/static/media/time-zone.b15b3746.svg"
+  },
+  {
+    "revision": "365dab8185cbc1443a1d57c522fc1cb5",
+    "url": "/Weather-app/static/media/uv.365dab81.svg"
+  },
+  {
+    "revision": "9ecb58e5cfb6175081e92c6039b67f1e",
+    "url": "/Weather-app/static/media/visibility.9ecb58e5.svg"
+  },
+  {
+    "revision": "7b8b2af526b2e2512c2682f889fef14f",
+    "url": "/Weather-app/static/media/water.7b8b2af5.svg"
+  },
+  {
+    "revision": "f435e6d9a8cefd6eaf82500eacf44df4",
+    "url": "/Weather-app/static/media/wind.f435e6d9.svg"
+  }
+]);
